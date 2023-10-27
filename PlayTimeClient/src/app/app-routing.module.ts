b792de62418c8563e4798be9f2ac4728354789f1
Martijn <oldmartijntje@gmail.com>
2023-10-27 12:44:46 +0200

    template

--- PlayTimeClient/src/app/app-routing.module.ts
+++ PlayTimeClient/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditorPageComponent } from './pages/editor-page/editor-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/Editor' },
+    { path: 'Editor', component: EditorPageComponent },
+    //   { path: 'Store/Category/:View', component: StorePageComponent },
+    //   { path: 'Datapage', component: DataPageComponent },
+
+    // { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- PlayTimeClient/src/app/app.module.ts
+++ PlayTimeClient/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToastPopupComponent } from './components/global/toast-popup/toast-popup.component';
+import { AngularSplitModule } from 'angular-split';
+import { EditorPageComponent } from './pages/editor-page/editor-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToastPopupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ToastPopupComponent,
+        EditorPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularSplitModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
