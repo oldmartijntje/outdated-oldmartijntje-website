29c1307d2fa49c7b82d62f091a1322a4e42f81e1
Martijn <oldmartijntje@gmail.com>
2023-11-10 16:22:44 +0100

    builder version

---
+++ scripts/builder.py
@@ -0,0 +1,173 @@
+exitMessage = "Press Enter key to exit..."
+
+try:
+    import json
+    import os
+    import subprocess
+    import shutil
+    from pathlib import Path
+    from datetime import datetime
+
+    JsonVersion = 2
+    BuilderVersion = 8
+
+    defaultData = {
+        "BuildNumber":1,
+        "JSONVersion":JsonVersion
+    }
+
+    current_datetime = datetime.now()
+
+    # Format the date and time as day/month/year: hour:minute:seconds in 24-hour format
+    formatted_datetime = current_datetime.strftime("%d/%m/%Y: %H:%M:%S")
+
+    res = subprocess.run(["git", "config", "user.name"], stdout=subprocess.PIPE)
+    git_username = res.stdout.strip().decode()
+
+    def create_or_read_builder_data():
+        filename = "scripts/BuilderData.json"
+
+        if os.path.isfile(filename):
+            # If the file exists, read the JSON data from it
+            with open(filename, 'r') as file:
+                builder_data = json.load(file)
+                # Increment the 'buildNumber' field by 1
+                builder_data["BuildNumber"] += 1
+        else:
+            # If the file doesn't exist, initialize it with default data
+            builder_data = defaultData
+
+        with open(filename, 'w') as file:
+            json.dump(builder_data, file, indent=4)
+
+        return builder_data
+    
+    def get_last_commit_id():
+        try:
+            # Run the 'git rev-parse HEAD' command to get the last commit ID
+            result = subprocess.check_output(["git", "rev-parse", "HEAD"], universal_newlines=True)
+            commit_id = result.strip()  # Remove any leading/trailing whitespace
+            return commit_id
+        except subprocess.CalledProcessError as e:
+            # Handle any errors, such as Git not being installed or not being in a Git repository
+            print("Error:", e)
+            return None
+
+    def update_build_data_file(buildNumber, jsonVersion, buildedCorrectly = True, error = None):
+        # Define the content to be written to the TypeScript file
+        if (error != None):
+            errorMsg = f'\n    "errorMessage": "{error}",'
+        else:
+            errorMsg = ""
+        content = f'''export const BuildData: any = {{
+        "BuildNumber": {buildNumber},
+        "Branch": "{branchName}",
+        "BuildedCorrectly": {f"{buildedCorrectly}".lower()},
+        "BuilderVersion": {BuilderVersion},
+        "JsonVersion": {jsonVersion},
+        "Builder": "{git_username}",{errorMsg}
+        "BuildDate": "{formatted_datetime}",
+        "LastCommitId": "{get_last_commit_id()}",
+        "LastCommitURL": "https://github.com/oldmartijntje/playtime/commit/{get_last_commit_id()}"
+    }};
+        '''
+
+        # Specify the path to the TypeScript file
+        file_path = 'PlayTimeClient/src/app/Models/buildData.ts'
+
+        try:
+            # Open the TypeScript file in write mode
+            with open(file_path, 'w') as file:
+                # Write the updated content to the file
+                file.write(content)
+
+        except Exception as e:
+            print(f'An error occurred while updating buildData.ts: {e}')
+
+    def get_active_branch_name():
+        try:
+            head_dir = Path(".") / ".git" / "HEAD"
+            with head_dir.open("r") as f: content = f.read().splitlines()
+
+            for line in content:
+                if line[0:4] == "ref:":
+                    return line.partition("refs/heads/")[2]
+        except Exception as e:
+            print(f'An error occurred while getting the active branch name: {e}')
+            return "Unknown"
+
+
+    current_working_directory = os.getcwd()
+
+    splitted = current_working_directory.split('\\')
+
+
+    while os.path.isdir("PlayTimeClient") == False and len(splitted) > 2:
+        os.chdir("..")
+        current_working_directory = os.getcwd()
+
+        splitted = current_working_directory.split('\\')
+
+    if (len(splitted) == 2 and splitted[1] == ''):
+        print("ERROR: PlayTimeClient folder not found")
+        input(exitMessage)
+        exit()
+
+    branchName = get_active_branch_name()
+    builder_data = create_or_read_builder_data()
+
+
+
+
+    try:
+
+
+        # Define the branch names
+        new_branch_name = f"Build/BuildId_{builder_data['BuildNumber']}_DateTime_{current_datetime.strftime("D%d-%m-%YT%H-%M-%S")}"
+        dev_branch_name = "main"
+
+        # Create a new branch
+        subprocess.run(["git", "checkout", "-b", new_branch_name])
+
+        update_build_data_file(builder_data["BuildNumber"], builder_data["JSONVersion"])
+
+        # Change the current working directory to the PistonClient folder
+        os.chdir('PlayTimeClient')
+
+        npm_build_command = 'npm install'
+        subprocess.run(npm_build_command, shell=True, check=True)
+
+        # Build the Angular project
+        ng_build_command = 'ng build --configuration "production" --base-href "https://oldmartijntje.github.io/playtime/"'
+        subprocess.run(ng_build_command, shell=True, check=True)
+
+        gh_pages_command = "npx angular-cli-ghpages --dir=dist/play-time"
+        subprocess.run(gh_pages_command, shell=True, check=True)
+
+        # Stage your changes (you can add your changes here)
+        # For example, you can use 'git add' to stage your changes:
+        # subprocess.run(["git", "add", "your_file_or_directory"])
+
+        # Stage all changes
+        subprocess.run(["git", "add", "."])
+        # Commit the staged changes
+        # Replace "Your commit message" with your actual commit message
+        subprocess.run(["git", "commit", "-m", f"{formatted_datetime}. Build: {builder_data['BuildNumber']}"])
+
+        # Push the new branch to the remote repository (replace "origin" with your remote name)
+        subprocess.run(["git", "push", "-u", "origin", new_branch_name])
+
+        # Switch back to the dev branch
+        subprocess.run(["git", "checkout", dev_branch_name])
+
+    except Exception as e:
+        print(f'An error occurred while building the project: {e}')
+        update_build_data_file(builder_data["BuildNumber"], builder_data["JSONVersion"], False, e)
+        input(exitMessage)
+        exit()
+    input(exitMessage)
+    exit()
+except Exception as ae:
+    print(f'An error occurred while building the project: {ae}')
+    input(exitMessage)
+    exit()
