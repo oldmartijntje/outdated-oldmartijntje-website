4878f9fe27233a476c2c7db75da8ed979c7e6642
Martijn <oldmartijntje@gmail.com>
2023-11-16 18:07:18 +0100

    start of bookmarks

---
+++ PlayTimeClient/src/app/data/bookmarks.ts
@@ -0,0 +1,61 @@
+export const bookmarks: Record<string, any>[] = [
+    {
+        "Title": "Codepen",
+        "Tabs": [
+            {
+                "Text": "Tab1",
+                "Buttons": [
+                    {
+                        "Text": "Reset Alarm...",
+                        "Command": "",
+                        "Link": "https://codepen.io/",
+                        "AfterText": "Try this to get some attention"
+                    },
+                    {
+                        "Text": "Button2",
+                        "Command": "",
+                        "Link": "https://codepen.io/",
+                        "AfterText": "AfterText2"
+                    }
+                ],
+                "Id": 0
+
+            },
+            {
+                "Text": "Tab2",
+                "Buttons": [
+                    {
+                        "Text": "Button3",
+                        "Command": "",
+                        "Link": "https://codepen.io/",
+                        "AfterText": "AfterText1"
+                    },
+                    {
+                        "Text": "Button4",
+                        "Command": "",
+                        "Link": "https://codepen.io/",
+                        "AfterText": "AfterText2"
+                    }
+                ],
+                "Id": 1
+
+            }
+        ],
+        "ActiveTabId": 0,
+        "Id": 0,
+        "Buttons": [
+            {
+                "Text": "OK",
+                "Command": "",
+                "Link": "",
+                "AfterText": "AfterText1"
+            },
+            {
+                "Text": "Cancel",
+                "Command": "",
+                "Link": "",
+                "AfterText": "AfterText2"
+            }
+        ]
+    }
+]
--- PlayTimeClient/src/app/pages/other/bookmarks-page/bookmarks-page.component.ts
+++ PlayTimeClient/src/app/pages/other/bookmarks-page/bookmarks-page.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { bookmarks } from 'src/app/data/bookmarks';
 
 @Component({
-  selector: 'app-bookmarks-page',
-  templateUrl: './bookmarks-page.component.html',
-  styleUrls: ['./bookmarks-page.component.scss']
+    selector: 'app-bookmarks-page',
+    templateUrl: './bookmarks-page.component.html',
+    styleUrls: ['./bookmarks-page.component.scss']
 })
 export class BookmarksPageComponent {
 
+    constructor(private router: Router) { }
+
+    bookmarks = bookmarks;
+
+    routerNav(routeSegments: string[]): void {
+        // Navigate to the specified route
+        this.router.navigate(routeSegments);
+    }
+
+    setActiveTab(tabId: number, bookmarkId: number): void {
+        this.bookmarks[bookmarkId]["ActiveTabId"] = tabId;
+    }
+
+    isActiveTab(tabId: string, bookmarkId: string, bookmark: any): boolean {
+        return bookmark['ActiveTabId'] == tabId;
+    }
+
+    deleteBookmark(bookmarkId: number): void {
+        this.bookmarks.splice(bookmarkId, 1);
+    }
 }
