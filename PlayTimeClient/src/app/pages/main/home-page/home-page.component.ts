44f858a00da5a8a4550f7922d1261f550f7a354e
Martijn <oldmartijntje@gmail.com>
2023-11-18 23:11:13 +0100

    home page

--- PlayTimeClient/src/app/pages/main/home-page/home-page.component.ts
+++ PlayTimeClient/src/app/pages/main/home-page/home-page.component.ts
@@ -5,13 +5,21 @@ import { BuildData } from 'src/app/models/buildData';
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
-    styleUrls: ['./home-page.component.scss']
+    styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
+    coinCounter = 0;
     versionNumber = BuildData["BuildNumber"];
     versionWord = "BuildId";
 
     logBuildData() {
         console.log(BuildData);
+    }
+
+    addCoin() {
+        this.coinCounter++;
+        if (this.coinCounter > 99) {
+            this.coinCounter = 0;
+        }
     }
 }
