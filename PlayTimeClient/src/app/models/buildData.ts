4ac91fac7e3d9b0100d5b357d0aacb6c6ab41825
Martijn <oldmartijntje@gmail.com>
2023-11-20 14:20:22 +0100

    20/11/2023: 14:19:33. Build: 24

--- PlayTimeClient/src/app/models/buildData.ts
+++ PlayTimeClient/src/app/models/buildData.ts
@@ -1,12 +1,12 @@
 export const BuildData: any = {
-        "BuildNumber": 23,
+        "BuildNumber": 24,
         "Branch": "main",
         "BuildedCorrectly": true,
         "BuilderVersion": 8,
         "JsonVersion": 2,
         "Builder": "Martijn",
-        "BuildDate": "20/11/2023: 10:57:51",
-        "LastCommitId": "e99e50b8d743810f7004a9739b312607f9f7d6f2",
-        "LastCommitURL": "https://github.com/oldmartijntje/playtime/commit/e99e50b8d743810f7004a9739b312607f9f7d6f2"
+        "BuildDate": "20/11/2023: 14:19:33",
+        "LastCommitId": "be07ddb3719310b42b2b528388116a28ca82b2a6",
+        "LastCommitURL": "https://github.com/oldmartijntje/playtime/commit/be07ddb3719310b42b2b528388116a28ca82b2a6"
     };
         
