a9681ed15ca196714f38b8c01cfd7aa5082690b8
Martijn <oldmartijntje@gmail.com>
2023-11-21 22:12:57 +0100

    would you look at that, the env.prod is gone

--- PlayTimeClient/src/environments/environmentt.prod.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-    production: true,
-    apiUrl: 'https://production-api-url.com/api', // Your production API URL
-    encryptionKey: 69,
-    encryptionModifier: 420
-};
