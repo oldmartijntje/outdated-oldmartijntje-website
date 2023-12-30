fafce9fa4f84892f27d112552a3be74bc69d957d
Martijn <oldmartijntje@gmail.com>
2023-12-30 16:07:11 +0100

    this <mat-autocomplete> works, now let's implement it

--- PlayTimeClient/src/app/pages/other/testing-page/testing-page.component.ts
+++ PlayTimeClient/src/app/pages/other/testing-page/testing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { debounceTime, map, startWith } from 'rxjs/operators';
@@ -8,43 +8,21 @@ import { debounceTime, map, startWith } from 'rxjs/operators';
     templateUrl: './testing-page.component.html',
     styleUrls: ['./testing-page.component.scss']
 })
-export class TestingPageComponent {
-    // Your data in the specified format
-    data: { [key: string]: { type: string } } = {
-        "id1": { "type": "prompt" },
-        "id2": { "type": "prompt" },
-        "id3": { "type": "prompt" },
-        "id4": { "type": "prompt" },
-        // Add more data as needed
-    };
-    test = [{ id: 'id1', type: 'prompt' }, { id: 'id2', type: 'prompt' }, { id: 'id3', type: 'prompt' }, { id: 'id4', type: 'prompt' }];
+export class TestingPageComponent implements OnInit {
+    myControl = new FormControl('');
+    options: string[] = ['One', 'Two', 'Three'];
+    filteredOptions: Observable<string[]> = new Observable<string[]>();
 
-    // Autocomplete variables
-    searchControl = new FormControl();
-    filteredData: Observable<any[]>;
-
-    constructor() {
-        // Initialize the filteredData observable
-        this.filteredData = this.searchControl.valueChanges.pipe(
+    ngOnInit() {
+        this.filteredOptions = this.myControl.valueChanges.pipe(
             startWith(''),
-            debounceTime(300), // Adjust the debounce time as needed
-            map(value => this._filter(value))
+            map(value => this._filter(value || '')),
         );
     }
 
-    // Function to filter the data based on user input
-    private _filter(value: string): any[] {
+    private _filter(value: string): string[] {
         const filterValue = value.toLowerCase();
-        const x = Object.keys(this.data).filter(key => key.toLowerCase().includes(filterValue))
-            .map(key => ({ id: key, type: this.data[key].type }));
-        console.log(x);
-        return x;
-    }
 
-    // Function to log filteredData
-    logFilteredData() {
-        this.filteredData.subscribe(data => {
-            console.log('Filtered Data:', data);
-        });
+        return this.options.filter(option => option.toLowerCase().includes(filterValue));
     }
 }
