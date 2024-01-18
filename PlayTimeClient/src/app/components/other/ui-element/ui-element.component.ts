379057cb18f2876ea6f61fa9bdc2273786ace43e
Martijn <oldmartijntje@gmail.com>
2024-01-18 16:31:59 +0100

    new scrollbar and ui outside page is designed

--- PlayTimeClient/src/app/components/other/ui-element/ui-element.component.ts
+++ PlayTimeClient/src/app/components/other/ui-element/ui-element.component.ts
@@ -1,20 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-ui-element',
     templateUrl: './ui-element.component.html',
     styleUrl: './ui-element.component.scss'
 })
-export class UiElementComponent {
+export class UiElementComponent implements OnInit {
     @Input() widthOfContent: string = '800px';
     @Input() maxWidthOfContent: string = 'none';
 
+    heightOfContent: string = '100%';
+    customWidthOfContent = this.widthOfContent;
+
     isWideScreen: boolean = window.innerWidth >= 1024;
 
-    constructor() {
-        // Listen to window resize events to update the flag
-        window.addEventListener('resize', () => {
-            this.isWideScreen = window.innerWidth >= 1024;
-        });
+    ngOnInit() {
+        this.customWidthOfContent = this.isBiggerThanMaxWidth();
+        this.onResize(null);
     }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: Event | null) {
+        this.isWideScreen = window.innerWidth >= 1024;
+        if (this.isWideScreen) {
+            this.heightOfContent = '100%';
+        } else {
+            this.heightOfContent = '90%';
+        }
+        console.log(this.heightOfContent, this.isWideScreen);
+        this.customWidthOfContent = this.isBiggerThanMaxWidth();
+    }
+
+    isBiggerThanMaxWidth(): string {
+        if (parseInt(this.maxWidthOfContent.split('px')[0]) > window.innerWidth) {
+            return window.innerWidth + 'px';
+        } else {
+            return this.widthOfContent;
+        }
+    }
+
+
 }
